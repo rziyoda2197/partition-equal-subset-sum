a0d56ddcd18a92eb26d07dbd95a56a010d1ed0f9
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-20 15:46:30 +0500

    🚀 Add main application code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function canPartition(nums) {
+    let sum = nums.reduce((a, b) => a + b, 0);
+    if (sum % 2 !== 0) return false;
+
+    let target = sum / 2;
+    let dp = new Array(target + 1).fill(false);
+    dp[0] = true;
+
+    for (let num of nums) {
+        for (let i = target; i >= num; i--) {
+            dp[i] = dp[i] || dp[i - num];
+        }
+    }
+
+    return dp[target];
+}
+
+// Test
+console.log(canPartition([1, 5, 11, 5])); // true
+console.log(canPartition([1, 2, 3, 5])); // false
